fa9ab3f65381ce7f151f10c59eb84dc0ea16866d
developer-Bushido <alkhamidov@ozon.ru>
2023-05-19 22:15:32 +0300

    [New Terraform] Let's update our infrastructure

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -3,55 +3,48 @@ provider "aws" {
   profile = "default"
 }
 
-# DB SERVERS
-
-resource "aws_instance" "my_DB_master" {
-  count                  = 1
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t3.micro"
-  key_name               = "admin_kp"
-  vpc_security_group_ids = [aws_security_group.db_security_group.id]
-  tags = {
-    Name = "DB Master Server"
-  }
-}
-
-resource "aws_instance" "my_DB_slave" {
+resource "aws_instance" "orchestrator" {
   count                  = 3
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
   key_name               = "admin_kp"
-  vpc_security_group_ids = [aws_security_group.db_security_group.id]
+  vpc_security_group_ids = [aws_security_group.orchestrator_security_group.id]
   tags = {
-    Name = "DB Slave Server"
+    Name = "Orchestrator"
   }
 }
 
-# WEB SERVERS
-
-resource "aws_instance" "my_WEB" {
+resource "aws_instance" "proxysql" {
+  count                  = 2
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
   key_name               = "admin_kp"
-  vpc_security_group_ids = [aws_security_group.web_server.id]
+  vpc_security_group_ids = [aws_security_group.proxysql_security_group.id]
   tags = {
-    Name = "Web and Monitoring"
+    Name = "ProxySQL"
   }
 }
 
+resource "aws_instance" "mariadb" {
+  count                  = 3
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  key_name               = "admin_kp"
+  vpc_security_group_ids = [aws_security_group.mariadb_security_group.id]
+  tags = {
+    Name = "MariaDB"
+  }
+}
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
   owners = ["099720109477"] # Canonical
 }
